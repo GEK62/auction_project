408f4b58d75e951a586cf83ac74d37c9ca8dc517
GEK62 <abasovwork@gmail.com>
2021-12-05 22:23:10 +0300

    Devise gem functionality connect

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  root 'home#index'
+  get 'home/index'
+  get 'home/about'
   devise_for :users
-  root 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
