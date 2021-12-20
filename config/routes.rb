a1146ab7c1253e5e0507d841fa9077d3158650af
GEK62 <abasovwork@gmail.com>
2021-12-21 00:36:01 +0300

    All test deleted

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,5 @@ Rails.application.routes.draw do
   get 'home/index'
   get 'home/about'
   get 'home/contact'
-  get 'lot/index'
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- test/controllers/lot_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class LotControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
